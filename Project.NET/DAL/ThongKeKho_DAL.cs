dc723e5f5a62c7bb6cf7ed86b1759c8eda3a14b1
Sok Kim Thanh <thanhsk1991@gmail.com>
2024-05-09 12:20:04 +0700

    add thong ke kho dal, bus, dto

---
+++ Project.NET/DAL/ThongKeKho_DAL.cs
@@ -0,0 +1,87 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ThongKeKho_DAL : BasicMethod<ThongKeKho_DTO>
+    {
+        public List<ThongKeKho_DTO> ThongKeSanPhamTheoLoaiSP_Kho_ChiNhanh(KhoCN khoCN)
+        {
+            List<ThongKeKho_DTO> list = null;
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return list;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public override IQueryable LayDanhSach()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Sua(ThongKeKho_DTO obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Them(ThongKeKho_DTO obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Xoa(string obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
